ecfc0aa94c785f78c6e1f649b1e82702e041e692
许林 <xulin.wh@qq.com>
2018-12-29 17:26:23 +0800

    init

---
+++ RainCat/RainCat/BackgroundNode.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  RainCat
+//
+//  Created by CoderDream on 2018/12/29.
+//  Copyright © 2018 CoderDream. All rights reserved.
+//
+
+import SpriteKit
+
+public class BackgroundNode : SKNode {
+    public func setup(size : CGSize) {
+        let yPos : CGFloat = size.height * 0.10
+        let startPoint = CGPoint(x: 0, y: yPos)
+        let endPoint = CGPoint(x: size.width, y: yPos)
+        physicsBody = SKPhysicsBody(edgeFrom: startPoint, to: endPoint)
+        physicsBody?.restitution = 0.3
+    }
+    
+}
